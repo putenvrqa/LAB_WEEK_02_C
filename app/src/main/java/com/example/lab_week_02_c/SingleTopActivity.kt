03d242d25375b76d8e5fde42535776d282e15123
putee <puyi.novariqa@gmail.com>
2025-09-08 02:28:51 +0700

    Commit No. 4 – Standard activity and single top activity

---
+++ app/src/main/java/com/example/lab_week_02_c/SingleTopActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lab_week_02_c
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SingleTopActivity : AppCompatActivity() {
+    companion object{
+        private const val DEBUG = "DEBUG"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_top)
+        Log.d(DEBUG, "onCreate")
+
+        findViewById<Button>(R.id.button_single_top).setOnClickListener{
+            startActivity(
+                Intent(this, SingleTopActivity::class.java)
+            )
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d(DEBUG, "onNewIntent")
+    }
+}
